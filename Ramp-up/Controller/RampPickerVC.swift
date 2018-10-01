475576bdc82f70dcc8147fe1033de1606587d84e
Martin <martinstojchev@hotmail.com>
2018-10-01 10:12:40 +0200

    Popover chooser controller added

---
+++ Ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,49 @@
+//
+//  RampPickerVC.swift
+//  Ramp-up
+//
+//  Created by Martin on 10/1/18.
+//  Copyright © 2018 Martin. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize){
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
