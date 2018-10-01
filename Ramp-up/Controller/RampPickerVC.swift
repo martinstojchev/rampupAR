637cf3c55618ae9305eb86f4c9b3606991dca7b4
Martin <martinstojchev@hotmail.com>
2018-10-01 10:59:50 +0200

    Pipe showing in the choosing popover

--- Ramp-up/Controller/RampPickerVC.swift
+++ Ramp-up/Controller/RampPickerVC.swift
@@ -32,6 +32,20 @@ class RampPickerVC: UIViewController {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")
+        sceneView.scene = scene
+        
+        //set the orthographic camera
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene?.rootNode.camera = camera
+        
+        let obj = SCNScene(named: "art.scnassets/pipe.dae")
+        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
+        //scale the node
+        node?.scale = SCNVector3Make(0.0012, 0.0012, 0.0012)
+        node?.position = SCNVector3Make(-0, 1.3, -1)
+        scene?.rootNode.addChildNode(node!)
         preferredContentSize = size
     }
     
